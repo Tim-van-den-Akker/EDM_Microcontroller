7011d1e75bd1c3c7ddee94a833b3c5f40beb4b3a
Tim van den Akker <timakk01@gmail.com>
2024-05-22 12:16:28 +0200

    removed master meuk

--- main.cpp
+++ main.cpp
@@ -196,48 +196,19 @@ void sample(uint8_t *capture_buf) {
 static const uint I2C_SLAVE_ADDRESS = 0x17;
 static const uint I2C_BAUDRATE = 100000; // 100 kHz
 
-// For this example, we run both the master and slave from the same board.
-// You'll need to wire pin GP4 to GP6 (SDA), and pin GP5 to GP7 (SCL).
 static const uint I2C_SLAVE_SDA_PIN = PICO_DEFAULT_I2C_SDA_PIN; // 4
 static const uint I2C_SLAVE_SCL_PIN = PICO_DEFAULT_I2C_SCL_PIN; // 5
-static const uint I2C_MASTER_SDA_PIN = 6;
-static const uint I2C_MASTER_SCL_PIN = 7;
 
-// The slave implements a 256 byte memory. To write a series of bytes, the master first
-// writes the memory address, followed by the data. The address is automatically incremented
-// for each byte transferred, looping back to 0 upon reaching the end. Reading is done
-// sequentially from the current memory address.
-static struct
-{
-    uint8_t mem[256];
-    uint8_t mem_address;
-    bool mem_address_written;
-} context;
-
-// Our handler is called from the I2C ISR, so it must complete quickly. Blocking calls /
-// printing to stdio may interfere with interrupt handling.
 static void i2c_slave_handler(i2c_inst_t *i2c, i2c_slave_event_t event) {
+    uint8_t data_send = (uint8_t) ((adc_read()) / 16);
     switch (event) {
     case I2C_SLAVE_RECEIVE: // master has written some data
-        // print data received to stdio
         printf("Received: %d\n", i2c_read_byte_raw(i2c));
-        // if (!context.mem_address_written) {
-        //     // writes always start with the memory address
-        //     context.mem_address = i2c_read_byte_raw(i2c);
-        //     context.mem_address_written = true;
-        // } else {
-        //     // save into memory
-        //     context.mem[context.mem_address] = i2c_read_byte_raw(i2c);
-        //     context.mem_address++;
-        // }
-        break;
     case I2C_SLAVE_REQUEST: // master is requesting data
-        // load from memory
-        i2c_write_byte_raw(i2c, context.mem[context.mem_address]);
-        context.mem_address++;
+        printf("Requested\n Sending %d\n", data_send);
+        i2c_write_byte_raw(i2c, data_send);
         break;
     case I2C_SLAVE_FINISH: // master has signalled Stop / Restart
-        context.mem_address_written = false;
         break;
     default:
         break;
@@ -258,19 +229,6 @@ static void setup_slave() {
     i2c_slave_init(i2c0, I2C_SLAVE_ADDRESS, &i2c_slave_handler);
 }
 
-static void setup_master() {
-    gpio_init(I2C_MASTER_SDA_PIN);
-    gpio_set_function(I2C_MASTER_SDA_PIN, GPIO_FUNC_I2C);
-    // pull-ups are already active on slave side, this is just a fail-safe in case the wiring is faulty
-    gpio_pull_up(I2C_MASTER_SDA_PIN);
-
-    gpio_init(I2C_MASTER_SCL_PIN);
-    gpio_set_function(I2C_MASTER_SCL_PIN, GPIO_FUNC_I2C);
-    gpio_pull_up(I2C_MASTER_SCL_PIN);
-
-    i2c_init(i2c1, I2C_BAUDRATE);
-}
-
 int main(){
 
     stdio_init_all(); 
@@ -278,7 +236,6 @@ int main(){
     // setup_adc();
 
     setup_slave();
-    setup_master();
 
     pulse_analyzer pulse_analyzer(26);
 
@@ -300,17 +257,8 @@ int main(){
     adc_select_input(0);
 
 
-    uint8_t data[1] = {0}; 
-
     while (1)
     {        
-
-        // read potentiometer value
-        data[0] = (uint8_t) ((adc_read()) / 16);
-
-
-        i2c_write_timeout_us(i2c1, I2C_SLAVE_ADDRESS, data, 1, true, 100);
-        gpio_put(25, 0);
         sleep_ms(10);
     }
   
